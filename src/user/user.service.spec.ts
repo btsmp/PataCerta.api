3628ad323569f46d5d5cbcbb860545e927e9cd5e
Bruno Sampiao B2x <brunoctsb@gmail.com>
2023-11-22 20:46:23 -0300

    test: crud test in user.service

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,14 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../shared/config/prisma';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const generateFakeUser = (): User => {
+  return {
+    id: faker.string.uuid(),
+    email: faker.internet.email(),
+    cpf: faker.number.int({ min: 11111111111, max: 99999999999 }).toString(),
+    name: faker.person.fullName(),
+    password: faker.internet.password({ length: 6 }),
+    isOng: faker.datatype.boolean(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isValidated: faker.datatype.boolean(),
+  };
+};
+
+const fakeUsers: User[] = Array.from({ length: 3 }, generateFakeUser);
 
 const prismaMock = {
   user: {
     create: jest.fn(),
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
     update: jest.fn(),
+    delete: jest.fn(),
   },
 };
+
 describe('UserService', () => {
   let sut: UserService;
 
@@ -32,7 +55,7 @@ describe('UserService', () => {
     expect(sut).toBeDefined();
   });
 
-  describe('method create', () => {
+  describe('create', () => {
     it('should be able to create a new user', async () => {
       const testUser: CreateUserDto = {
         email: 'test@example.com',
@@ -64,13 +87,85 @@ describe('UserService', () => {
 
       try {
         await sut.create(testUser);
-        fail('Expected exception was not thrown');
       } catch (error) {
         expect(error).toBeInstanceOf(BadRequestException);
         expect(error.message).toBe('Invalid input data');
       }
-
       expect(prismaMock.user.create).not.toHaveBeenCalled();
     });
+  });
+
+  describe('list', () => {
+    it('should be able to list all users', async () => {
+      prismaMock.user.findMany.mockReturnValue(fakeUsers);
+      const response = await sut.findAll();
+      expect(response).toBeDefined();
+      expect(response).toEqual(fakeUsers);
+      expect(prismaMock.user.findMany).toHaveBeenCalled();
+    });
+
+    it('should be able to find one specific user by id', async () => {
+      prismaMock.user.findFirst.mockReturnValue(fakeUsers[2]);
+      const id = fakeUsers[2].id;
+      const response = await sut.findOne(id);
+      expect(response).toBeDefined();
+      expect(response).toMatchObject(fakeUsers[2]);
+      expect(prismaMock.user.findFirst).toHaveBeenCalled();
+    });
+
+    it('should throw an error if id provided is invalid', async () => {
+      const id = faker.string.uuid();
+      try {
+        await sut.findOne(id);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toBe(`User ${id} does not exist`);
+      }
+      expect(prismaMock.user.findFirst).toHaveBeenCalled();
+      expect(prismaMock.user.findFirst).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should be able to update a field', async () => {
+      const existingUser: User = generateFakeUser();
+      const userUpdateMock: UpdateUserDto = {
+        email: 'teste2@gmail.com',
+      };
+      prismaMock.user.findFirst.mockResolvedValue(existingUser);
+      prismaMock.user.update.mockResolvedValue({
+        ...existingUser,
+        ...userUpdateMock,
+      });
+      const updatedUser = await sut.update(existingUser.id, userUpdateMock);
+
+      expect(updatedUser).toBeDefined();
+      expect(updatedUser).toEqual({ ...existingUser, ...userUpdateMock });
+
+      expect(prismaMock.user.update).toHaveBeenCalled();
+      expect(prismaMock.user.update).toHaveBeenCalledTimes(1);
+
+      expect(prismaMock.user.findFirst).toHaveBeenCalled();
+    });
+
+    it('should throw an error if user to update is not found', async () => {
+      const nonExistentUserId = faker.string.uuid();
+      const updatedUserData: UpdateUserDto = {
+        email: faker.internet.email(),
+      };
+      prismaMock.user.findFirst.mockResolvedValue(null);
+      try {
+        await sut.update(nonExistentUserId, updatedUserData);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toBe(`User ${nonExistentUserId} does not exist`);
+      }
+      expect(prismaMock.user.update).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('delete', () => {
+    it('should be able to delete a user', async () => {});
+    it('should not be able to delete a user if a non existent user', async () => {});
   });
 });
